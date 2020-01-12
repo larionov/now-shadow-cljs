4794d437496d97aaac585d27d55542e306824495
Sergey Larionov <sergey.larionov@gmail.com>
2020-01-12 15:59:38 +0700

    update node version to 12

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ async function createLambdaForNode(buildConfig, lambdas, workPath) {
   const lambda = await createLambda({
     files: { ...preparedFiles },
     handler: 'launcher.launcher',
-    runtime: 'nodejs8.10'
+    runtime: 'nodejs12.0'
   });
 
   lambdas[buildConfig.outputTo] = lambda;
